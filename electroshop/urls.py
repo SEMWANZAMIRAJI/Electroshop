2f6c834c1adedd25c63a914dce5af029bfdd7b05
unknown <semwanzamiraji@gmail.com>
2025-08-18 22:34:31 +0300

    mydepyiouiunbcvw

--- electroshop/urls.py
+++ electroshop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.contrib.sitemaps.views import sitemap
+
  # replace 'store' with your app name if different
 
 
